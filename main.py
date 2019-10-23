f70735b7bc7d786763b33a255b890e07de856f77
KalilCazes <kalilcazes@gmail.com>
2019-10-23 16:07:19 -0300

    Formatando impressao de relatório de simulaçao

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3.5
 import sys
+import fifo
 import lru
+import opt
 
 
 def main():
-
     n_quadros = int(sys.argv[1])
 
     sequencia_paginas = []
     for pagina in sys.stdin:
         sequencia_paginas.append(int(pagina))
 
-    print(lru.simulador(n_quadros, sequencia_paginas))
+    print(str(n_quadros) + " quadros, " + str(len(sequencia_paginas)) + " refs: FIFO: " + str(
+        fifo.simulador(n_quadros, sequencia_paginas)) + "PFs, " +
+          "LRU: " + str(lru.simulador(n_quadros, sequencia_paginas)) + "PFs, OPT: " + str(
+        opt.simulador(n_quadros, sequencia_paginas)) + "PFs")
 
 
 if __name__ == "__main__":
